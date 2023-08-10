4a9327b8368920c7e6b790a520c1e610c6cd4e87
ivan.polovyi <polovyivan@yahoo.com>
2023-08-09 23:15:36 -0300

    add postman collection

--- src/main/java/com/polovyi/ivan/tutorials/repository/SensorRepository.java
+++ src/main/java/com/polovyi/ivan/tutorials/repository/SensorRepository.java
@@ -1,22 +1,8 @@
 package com.polovyi.ivan.tutorials.repository;
 
 import com.polovyi.ivan.tutorials.dto.entity.SensorEntity;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
 import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.data.cassandra.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SensorRepository extends CassandraRepository<SensorEntity, String> {
-
-    @Query("""
-            SELECT * FROM sensor_events WHERE 
-            account_name=:#{#accountName} AND 
-            device_id=:#{#deviceId} AND
-            event_date = :#{#eventDate} AND 
-            event_id = :#{#eventId} 
-            """)
-    List<SensorEntity> findByKeyAccountNameAndKeyDeviceId(String accountName, UUID deviceId,  LocalDate eventDate, UUID eventId);
-}
+public interface SensorRepository extends CassandraRepository<SensorEntity, String> { }
